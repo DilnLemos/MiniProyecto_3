44290c61b2b377791dd780b54d40f7e088638d6c
JuanJoseR18 <juan.restrepo.avalo@correounivalle.edu.co>
2024-06-06 20:03:54 -0500

    Archivo creado

---
+++ MiniProyecto_3/src/Vista/VistaJuegoPPTGUI.java
@@ -0,0 +1,14 @@
+package Vista;
+
+import javax.swing.*;
+//import Controlador.Controlador;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+// Representa la interfaz gráfica de usuario (GUI) para el juego de Piedra, Papel, Tijeras (PPT) con los elementos Fuego, Agua y Aire.
+
+public class VistaJuegoPPTGUI extends JFrame {
+
+}
